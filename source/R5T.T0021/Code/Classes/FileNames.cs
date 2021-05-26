dec8621929ccca0a562a3f0769b56cf14b53ab21
David Coats <david@getsafetycone.com>
2021-05-26 11:04:57 -0700

    Add FileNames class.

---
+++ source/R5T.T0021/Code/Classes/FileNames.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace R5T.T0021
+{
+    /// <summary>
+    /// Non-static class to be used as an extension method base for file names.
+    /// </summary>
+    public class FileNames
+    {
+        #region Static
+
+        public static FileNames Instance { get; } = new FileNames();
+
+        #endregion
+    }
+}
